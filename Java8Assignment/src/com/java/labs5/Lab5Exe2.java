318aef54e49507f0628813623f8f0eaef50f82d3
SonuDabhade <dabhadesonali463@gmail.com>
2021-06-08 16:36:06 +0530

    Commit

--- Java8Assignment/src/com/java/labs5/Lab5Exe2.java
+++ Java8Assignment/src/com/java/labs5/Lab5Exe2.java
@@ -1,5 +1,21 @@
 package com.java.labs5;
 
+import java.util.Scanner;
+
+class NameNotValidException extends Exception
+{
+     public String validname()
+     {
+          return ("Name is not Valid..Please ReEnter the Name");
+     }
+}
+class BlankNameNotValidException extends Exception
+{
+     public String blankName()
+     {
+          return ("Name should not be Blanked");
+     }
+}
 class Employee
 {
      
@@ -27,34 +43,44 @@ class Employee
                if(temp==1)
                     throw new NameNotValidException();
                else
-                    name=n;
+                    firstName=n;
           }
           catch(NameNotValidException e2)
           {
                System.out.println(e2);
           }
           /*———-Checking Age——————–*/
-          try
-          {
-               if(a>=15 && a<=21)
-                    age=a;
-               else
-                    throw new AgeNotWithInRangeException();
-          }
-          catch(AgeNotWithInRangeException e1)
-          {
-               System.out.println(e1);
-          }
+          
+         try {
+        	 if(firstName == " " || lastName== " ")
+        		 throw new BlankNameNotValidException();
+        	 else
+        		 System.out.println("first name, Last Name Entered");
+        	 
+         }
+         catch(BlankNameNotValidException e3){
+        	 
+         }
+          
      }
      void display()
      {
-          System.out.println("roll Name Age Course");
+          System.out.println(" FirstName LastName");
           System.out.println("---------------------");
-          System.out.println(roll+" "+name+" "+age+" " +course);
+          System.out.println(firstName+" "+lastName);
      }
 }
+
 public class Lab5Exe2 {
         public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
 			
+			System.out.print("\nEnter First name: ");
+	        String name = sc.next();
+	        System.out.print("\nEnter Last name: ");
+	        String name2 = sc.next();
+	        
+	        Employee s = new Employee(name, name2);
+	          s.display();
 		}
 }
